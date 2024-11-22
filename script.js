087c226bb51380cee1575d19382c3c1808fde416
gitahmad2004 <gitahmad2@gmail.com>
2024-11-22 02:29:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const cardwrapper = document.querySelector(".feposts");
+const cardwrapperChildren = Array.from(cardwrapper.children);
+const widthToScroll = cardwrapper.children[0].offsetWidth;
+const arrowPov = document.querySelector("#fleft");
+const arrowNext = document.querySelector("#fright");
+
+arrowPov.onclick = function () {
+    cardwrapper.scrollLeft -= widthToScroll;
+    updateButtonOpacity(); 
+}
+arrowNext.onclick = function () {
+    cardwrapper.scrollLeft += widthToScroll;
+    updateButtonOpacity(); 
+}
+
+
+function updateButtonOpacity() {
+
+    if (cardwrapper.scrollLeft <= 0) {
+        arrowPov.style.opacity = 0.4;
+        arrowPov.style.cursor = 1;
+    }
+
+    else if (cardwrapper.scrollLeft >= (cardwrapper.widthToScroll - cardwrapper.offsetWidth)) {
+        arrowNext.style.opacity = 0.4;
+        arrowNext.style.cursor = 1;
+    }
+    else {
+           arrowNext.style.opacity = 1;
+           arrowPov.style.opacity = 1;
+    }
+}
+
+
+
+
+
+const navBtn = document.querySelector(".menu");
+const navList = document.querySelector(".nav-list");
+
+navBtn.addEventListener("click", () => {
+  navList.classList.toggle("active");
+});
